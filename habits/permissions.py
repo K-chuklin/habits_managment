87245441f94909e5504fadf0637141cbeac975ba
Chuklin_ka <inkarttatoo@gmail.com>
2023-12-20 23:54:38 +0500

    fixes

--- habits/permissions.py
+++ habits/permissions.py
@@ -1,16 +1,6 @@
 from rest_framework.permissions import BasePermission
 
 
-class IsOwnerOrStaff(BasePermission):
-    message = 'Вы не являетесь владельцем или модератормом!'
-
-    def has_permission(self, request, view):
-        if request.user.is_staff:
-            return True
-
-        return request.user == view.get_object().owner
-
-
 class IsOwner(BasePermission):
     message = 'Вы не являетесь владельцем!'
 
--- habits/serializers.py
+++ habits/serializers.py
@@ -4,18 +4,18 @@ from habits.models import Habit
 
 class HabitSerializer(serializers.ModelSerializer):
 
-    def validate_time_of_completation(self, value):
+    def validate_time_to_complete(self, value):
         if value > 120:
-            raise serializers.ValidationError("Время выполнения привычки не должно превышать 120 сек.")
+            raise serializers.ValidationError("Время выполнения привычки не должно превышать 120 сек")
         return value
 
-    def validate_frequency(self, value):
+    def validate_freq(self, value):
         if value > 7:
-            raise serializers.ValidationError("Привычку необходимо выполнять не реже, чем раз в неделю.")
+            raise serializers.ValidationError("Привычку необходимо выполнять не реже, чем раз в неделю")
         return value
 
     def validate_associated_habit(self, value):
-        if value and not value.is_pleasant:
+        if value and not value.is_positive:
             raise serializers.ValidationError("Связанная привычка должна быть полезной")
         return value
 
@@ -28,7 +28,7 @@ class HabitSerializer(serializers.ModelSerializer):
             raise serializers.ValidationError("Связанную привычку и вознаграждение нельзя выбирать одновременно")
         elif is_positive and associated_habit or award:
             raise serializers.ValidationError(
-                "У приятной привычки не может быть вознаграждения или связанной привычки."
+                "У приятной привычки не может быть вознаграждения или связанной привычки"
             )
         return data
 
